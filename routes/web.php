87218f0f4902c35248ed68eb7cf908ad4846866e
Hamadnasir1999 <148016466+Hamadnasir1999@users.noreply.github.com>
2024-07-23 14:35:48 +0300

    second

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::view('/contact', 'contact')->name('contact');
+Route::view('/about', 'about')->name('about');
+Route::get('posts/{post}', [PostController::class, 'show'])->name('post.show');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
